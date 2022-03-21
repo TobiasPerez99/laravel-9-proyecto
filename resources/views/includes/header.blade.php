bd775915bdf3ea85d16cdc4ca7df3f9eeccfa5ce
Tobias Leonel Perez <tobiasleonel77@gmail.com>
2022-03-20 23:31:01 -0300

    agregamos botones para login o logout

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -10,6 +10,13 @@
             <li class="nav-item"><a href="#" class="nav-link">Unidades</a></li>
             <li class="nav-item"><a href="#" class="nav-link">Nosotros</a></li>
             <li class="nav-item"><a href="#" class="nav-link">Contactanos</a></li>
+            @guest
+            <li class="nav-item"><a href="{{route('login')}}" class="nav-link">Login</a></li>
+            @endguest
+            @auth
+            <li class="nav-item"><a href="{{route('logout')}}" class="nav-link">Logout</a></li>
+            @endauth
+
           </ul>
     </header>
 
